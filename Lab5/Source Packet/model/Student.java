e6a58c4d2b65078bdd892c72fdf22b03b0449661
nghiadz2110 <chinhghiaglai@gmail.com>
2025-11-21 21:12:18 +0700

    Add files via upload

---
+++ Lab5/Source Packet/model/Student.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author NGHIA
+ */
+import java.sql.Timestamp;
+
+public class Student {
+    private int id;
+    private String studentCode;
+    private String fullName;
+    private String email;
+    private String major;
+    private Timestamp createdAt;
+    public Student() {
+    }
+    public Student(String studentCode, String fullName, String email, String major) {
+        this.studentCode = studentCode;
+        this.fullName = fullName;
+        this.email = email;
+        this.major = major;
+    }
+
+    public Student(int id, String studentCode, String fullName, String email, String major, Timestamp createdAt) {
+        this.id = id;
+        this.studentCode = studentCode;
+        this.fullName = fullName;
+        this.email = email;
+        this.major = major;
+        this.createdAt = createdAt;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getStudentCode() {
+        return studentCode;
+    }
+    public void setStudentCode(String studentCode) {
+        this.studentCode = studentCode;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getMajor() {
+        return major;
+    }
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", studentCode='" + studentCode + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", major='" + major + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
